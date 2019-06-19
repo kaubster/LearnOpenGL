f437e0f547a5c5166f978baf1b4c53959928cb48
kaubin <kaubin@sonalysts.com>
2019-06-18 20:43:40 -0400

    playing

---
+++ src/7.in_practice/4.levels/game.cpp
@@ -0,0 +1,101 @@
+/*******************************************************************
+** This code is part of Breakout.
+**
+** Breakout is free software: you can redistribute it and/or modify
+** it under the terms of the CC BY 4.0 license as published by
+** Creative Commons, either version 4 of the License, or (at your
+** option) any later version.
+******************************************************************/
+#include "game.h"
+#include "resource_manager.h"
+#include "sprite_renderer.h"
+#include "game_object.h"
+#include <learnopengl/filesystem.h>
+
+
+// Game-related State data
+SpriteRenderer  *Renderer;
+GameObject      *Player;
+
+
+Game::Game(GLuint width, GLuint height) 
+	: State(GAME_ACTIVE), Keys(), Width(width), Height(height) 
+{ 
+
+}
+
+Game::~Game()
+{
+    delete Renderer;
+    delete Player;
+}
+
+void Game::Init()
+{
+    // Load shaders
+    ResourceManager::LoadShader(FileSystem::getPath("src/7.in_practice/3.rendering_sprites/sprite.vs").c_str(), FileSystem::getPath("src/7.in_practice/3.rendering_sprites/sprite.frag").c_str(), nullptr, "sprite");
+    
+	// Configure shaders
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(this->Width), static_cast<GLfloat>(this->Height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::GetShader("sprite").Use().SetInteger("sprite", 0);
+    ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+    // Load textures
+    ResourceManager::LoadTexture(FileSystem::getPath("resources/textures/background.jpg").c_str(), GL_FALSE, "background");
+    ResourceManager::LoadTexture(FileSystem::getPath("resources/textures/awesomeface.png").c_str(), GL_TRUE, "face");
+    ResourceManager::LoadTexture(FileSystem::getPath("resources/textures/block.png").c_str(), GL_FALSE, "block");
+    ResourceManager::LoadTexture(FileSystem::getPath("resources/textures/block_solid.png").c_str(), GL_FALSE, "block_solid");
+    ResourceManager::LoadTexture(FileSystem::getPath("resources/textures/paddle.png").c_str(), true, "paddle");
+    // Set render-specific controls
+    Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+    // Load levels
+    GameLevel one; one.Load(FileSystem::getPath("levels/one.lvl").c_str(), this->Width, this->Height * 0.5);
+    GameLevel two; two.Load(FileSystem::getPath("levels/two.lvl").c_str(), this->Width, this->Height * 0.5);
+    GameLevel three; three.Load(FileSystem::getPath("levels/three.lvl").c_str(), this->Width, this->Height * 0.5);
+    GameLevel four; four.Load(FileSystem::getPath("levels/four.lvl").c_str(), this->Width, this->Height * 0.5);
+    this->Levels.push_back(one);
+    this->Levels.push_back(two);
+    this->Levels.push_back(three);
+    this->Levels.push_back(four);
+    this->Level = 0;
+    // Configure game objects
+    glm::vec2 playerPos = glm::vec2(this->Width / 2 - PLAYER_SIZE.x / 2, this->Height - PLAYER_SIZE.y);
+    Player = new GameObject(playerPos, PLAYER_SIZE, ResourceManager::GetTexture("paddle"));
+}
+
+void Game::Update(GLfloat dt)
+{
+    
+}
+
+
+void Game::ProcessInput(GLfloat dt)
+{
+    if (this->State == GAME_ACTIVE)
+    {
+        GLfloat velocity = PLAYER_VELOCITY * dt;
+        // Move playerboard
+        if (this->Keys[GLFW_KEY_A])
+        {
+            if (Player->Position.x >= 0)
+                Player->Position.x -= velocity;
+        }
+        if (this->Keys[GLFW_KEY_D])
+        {
+            if (Player->Position.x <= this->Width - Player->Size.x)
+                Player->Position.x += velocity;
+        }
+    }
+}
+
+void Game::Render()
+{
+    if (this->State == GAME_ACTIVE)
+    {
+        // Draw background
+        Renderer->DrawSprite(ResourceManager::GetTexture("background"), glm::vec2(0, 0), glm::vec2(this->Width, this->Height), 0.0f);
+        // Draw level
+        this->Levels[this->Level].Draw(*Renderer);
+        // Draw player
+        Player->Draw(*Renderer);
+    }
+}
